2e5f2a9d1132b8831f86d974329c8686d49ff2fd
DorBergel <84246827+DorBergel@users.noreply.github.com>
2023-11-23 17:00:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,74 +1,139 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#define BUFFER_SIZE 25
 
-#define BUFFER_SIZE 50
-
-
-int logIn()
-{
-	FILE* fp = fopen("cardentials.txt", "r");
+typedef struct User {
+	char mail[BUFFER_SIZE];
 	char password[BUFFER_SIZE];
-	char email[BUFFER_SIZE];
-	char currPassword[BUFFER_SIZE];
-	char currEmail[BUFFER_SIZE];
-	char line[BUFFER_SIZE * 2];
-	int flag = 0;
+	int id;
+} User;
 
-	printf("Please enter your email: ");
-	scanf("%s", email);
-	printf("Please enter your password: ");
-	scanf("%s", password);
+int mailExist(char* m);
+int countLines();
+int signUp();
+
+void main()
+{
+	signUp();
+}
 
 
-	while (fgets(line, BUFFER_SIZE*2, fp)) {
-		
-		sscanf(line, "%[^,],%[^\n]", currEmail, currPassword);
-		
-		// DEBUG section
-		/*
-		printf("input_mail = %s     curr_mail = %s       strcmp: %d \n", email, currEmail, strcmp(email, currEmail));
-		printf("input_password = %s    curr_password = %s       strcmp: %d\n", password, currPassword, strcmp(password, currPassword));
+int login()
+{
+
+}
+
+int signUp()
+{
+	/*
+	This function get the new user cardentials, check if the mail already mailExist,
+	generate ID and store the data in the users text file.
+	Input: none
+	Output: 1 if there was an error, 0 else
+	*/
+
+	User* newUser = malloc(sizeof(User));
+	FILE* fp;
+
+	system("CLS");
+
+	printf("Email: ");
+	scanf("%s", newUser->mail);
+	printf("Password: ");
+	scanf("%s", newUser->password);
+
+	if (mailExist(newUser->mail)) {
+		system("CLS");
+		printf("Email already exist\n");
+		return 1;
+	}
+	else {
+		newUser->id = countLines() + 1;
+		fp = fopen("users.txt", "a");
+		fprintf(fp, "%s,%s,%d\n", newUser->mail, newUser->password, newUser->id);
+		fclose(fp);
+		system("CLS");
+		printf("Congrates, You Signed Up!\n");
+	}
+
+	return 0;
+}
+
+int mailExist(char* m)
+{
+	FILE* fp = fopen("users.txt", "r");
+	User* curr = malloc(sizeof(User));
+
+	while (fscanf(fp, "%[^,],%[^,],%d\n", curr->mail, curr->password, &curr->id) == 3){
+		/* DEBUG
+		printf("Now chacking: \n");
+		printf("%s\n", curr->mail);
+		printf("%s\n", curr->password);
+		printf("%d\n", curr->id);
 		printf("---------------------------------\n");
 		*/
-
-		if (strcmp(email, currEmail) == 0 && strcmp(password, currPassword) == 0) {
-			flag = 1;
-			break;
+		if (strcmp(m, curr->mail) == 0) {
+			fclose(fp);
+			free(curr);
+			return 1;
 		}
 	}
 
 	fclose(fp);
-	printf("Flag: %d", flag);
-	return flag;
+	free(curr);	
+
+	return 0;
 }
 
-void signUp()
+
+int userExist(char* m, char* p)
 {
-	FILE* fp = fopen("cardentials.txt", "a");
-	char password[BUFFER_SIZE];
-	char email[BUFFER_SIZE];
+	FILE* fp = fopen("users.txt", "r");
+	User* curr = malloc(sizeof(User));
 
-
-	system("CLS");
-	printf("Please enter your email: ");
-	scanf("%s", email); 
-	//email[strlen(email) - 1] = '\0';
-
-	printf("Please enter your password: ");
-	scanf("%s", password);
-	//password[strlen(password) - 1] = '\0';
-
-	fprintf(fp, "\n%s,%s", email, password);
-
-
-	system("CLS");
-	printf("Thank You for sign up\n");
+	while (fscanf(fp, "%[^,],%[^,],%d\n", curr->mail, curr->password, &curr->id) == 3) {
+		/* DEBUG
+		printf("Now chacking: \n");
+		printf("%s\n", curr->mail);
+		printf("%s\n", curr->password);
+		printf("%d\n", curr->id);
+		printf("---------------------------------\n");
+		*/
+		if (strcmp(m, curr->mail) == 0 && strcmp(p, curr->password) == 0) {
+			fclose(fp);
+			free(curr);
+			return 1;
+		}
+	}
 
 	fclose(fp);
+	free(curr);
+
+	return 0;
 }
 
-void main()
-{
-	logIn();
+
+int countLines() {
+	int count = 0;
+	char ch;
+	FILE* file = fopen("users.txt", "r");
+
+	// Read each character in the file
+	while ((ch = fgetc(file)) != EOF) {
+		// Increment count for each newline character
+		if (ch == '\n') {
+			count++;
+		}
+	}
+
+	// Increment count for the last line if not ending with newline
+	if (ch != '\n' && count > 0) {
+		count++;
+	}
+
+	fclose(file);
+
+	return count;
 }
